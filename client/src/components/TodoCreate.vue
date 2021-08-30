8aadfbd2f3887e9f4ac9fd8bfa12506f2402069d
VasiliyNefedov <nefedov.vasiliy@gmail.com>
2021-08-30 09:58:30 +0500

    минимальная верстка главной страницы

--- client/src/components/TodoCreate.vue
+++ client/src/components/TodoCreate.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <input type="text" v-model="newTodoTitle">
-    <textarea id="" cols="30" name="todo-body" rows="3" v-model="newTodoDescription"></textarea>
+  <div class="container">
+    <label for="title">Название задачи</label>
+    <input id="title" v-model="newTodoTitle">
+    <label for="description">Описание</label>
+    <textarea id="description" cols="30" name="todo-body" rows="3" v-model="newTodoDescription"></textarea>
     <button @click="addTodo">Создать</button>
   </div>
 </template>
@@ -27,5 +29,18 @@ export default {
 </script>
 
 <style scoped>
-
+.container {
+  margin: 0 auto;
+  max-width: 800px;
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+}
+.container > label {
+  padding: 10px;
+}
+button {
+  margin: 10px auto;
+  width: 200px;
+}
 </style>
--- client/src/components/TodoList.vue
+++ client/src/components/TodoList.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <div v-for="item in todoList" :key="item.id">
-      <p><strong>{{ item.title }}</strong></p>
+    <div v-for="item in todoList" :key="item.id" class="todo">
+      <div class="todo__head">
+        <div><label>Выполнено</label><input type="checkbox"></div>
+        <div class="todo__title"><p><strong>{{ item.title }}</strong></p></div>
+        <div>
+          <button class="del-btn">Удалить</button>
+        </div>
+      </div>
       <p>{{ item.description }}</p>
     </div>
   </div>
@@ -23,5 +29,21 @@ export default {
 </script>
 
 <style scoped>
-
+.todo {
+  margin: 10px auto;
+  width: 90%;
+  max-width: 800px;
+  border: lightcoral 3px solid;
+  border-radius: 10px;
+}
+.todo__head {
+  display: flex;
+  flex-direction: row;
+}
+.todo__title {
+  flex:1 1 auto;
+}
+.del-btn {
+  width: 80px;
+}
 </style>
